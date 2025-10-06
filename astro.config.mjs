5eb31aca14705aa3623e26a3220b8bbe20372bf9
Alec Reynolds <1153738+reynoldsalec@users.noreply.github.com>
2025-10-05 20:39:16 -0700

    Second try at GitHub Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://reynoldsalec.github.io',
+  site: 'https://www.alecreynolds.com',
   base: '/sol-cohousing-website',
 });
