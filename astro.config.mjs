ecd493e47fe9e51926fec1e0af17345fe4c5f3a5
Alec Reynolds <1153738+reynoldsalec@users.noreply.github.com>
2025-10-14 19:18:04 -0700

    Get rid of base URL config.

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://www.alecreynolds.com',
-  base: '/sol-village-website',
-});
